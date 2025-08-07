1f640b52a20a60124c4937de9c861855b2dcc89a
josuerks <ksjosuer@gmail.com>
2025-08-07 17:03:55 +0200

    Add Flutter web build files

--- web-build/flutter_bootstrap.js
+++ web-build/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2263203205"
+    serviceWorkerVersion: "150035992"
   }
 });
